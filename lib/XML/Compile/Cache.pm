85a920fd297e813cc17eb3b920184c09c6a477bc
Mark Overmeer <mark@overmeer.net>
2008-06-12 12:38:10 +0000

    Import of MARKOV/XML-Compile-Cache-0.11 from CPAN.
    gitpan-cpan-distribution: XML-Compile-Cache
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         MARKOV/XML-Compile-Cache-0.11.tar.gz
    gitpan-cpan-author:       MARKOV
    gitpan-cpan-maturity:     released

--- lib/XML/Compile/Cache.pm
+++ lib/XML/Compile/Cache.pm
@@ -1,13 +1,13 @@
 # Copyrights 2008 by Mark Overmeer.
 #  For other contributors see ChangeLog.
 # See the manual pages for details on the licensing terms.
-# Pod stripped from pm file by OODoc 1.04.
+# Pod stripped from pm file by OODoc 1.05.
 use warnings;
 use strict;
 
 package XML::Compile::Cache;
 use vars '$VERSION';
-$VERSION = '0.10';
+$VERSION = '0.11';
 
 use base 'XML::Compile::Schema';
 
@@ -68,32 +68,6 @@ sub allowUndeclared(;$)
 #----------------------
 
 
-sub declare($$@)
-{   my ($self, $need, $names, @opts) = @_;
-    my $opts = @opts==1 ? shift @opts : \@opts;
-    $opts = [ %$opts ] if ref $opts eq 'HASH';
-
-    my ($need_r, $need_w) = $self->_need($need);
-
-    foreach my $name (ref $names eq 'ARRAY' ? @$names : $names)
-    {   my $type = $self->findName($name);
-        if($need_r)
-        {   defined $self->{XCC_dropts}{$type}
-               and warning __x"reader {name} declared again", name => $name;
-            $self->{XCC_dropts}{$type} = $opts;
-        }
-
-        if($need_w)
-        {   defined $self->{XCC_dwopts}{$type}
-               and warning __x"writer {name} declared again", name => $name;
-            $self->{XCC_dwopts}{$type} = $opts;
-        }
-    }
-
-    $self;
-}
-
-
 sub compileAll(;$$)
 {   my ($self, $need, $usens) = @_;
     my ($need_r, $need_w) = $self->_need($need);
@@ -118,64 +92,6 @@ sub compileAll(;$$)
         }
     }
 }
-
-
-sub findName($)
-{   my ($self, $name) = @_;
-    my ($type, $ns, $local);
-    if($name =~ m/^\{/)
-    {   ($type, $ns, $local) = ($name, unpack_type $name);
-    }
-    elsif($name =~ m/^(\w+)\:(\S+)$/)
-    {   (my $prefix, $local) = ($1, $2);
-        $ns = $self->{XCC_prefix}{$prefix}
-            or error __x"unknown protocol name prefix `{prefix}'"
-                 , prefix => $prefix;
-
-        $type  = pack_type $ns, $local;
-    }
-    else
-    {   error __x"protocol name must show namespace or prefix";
-    }
-
-    $type;
-}
-
-
-sub printIndex(@)
-{   my $self = shift;
-    my $fh   = @_ % 2 ? shift : select;
-    my %args = @_;
-    my $decl = exists $args{show_declared} ? delete $args{show_declared} : 1;
-
-    return $self->SUPER::printIndex($fh, %args)
-        unless $decl;
-
-    my $output = '';
-    open my($out), '>', \$output;
-
-    $self->SUPER::printIndex($out, %args);
-
-    close $out;
-    my @output = split /(?<=\n)/, $output;
-    my $ns     = '';
-    foreach (@output)
-    {   $ns = $1 if m/^namespace\:\s+(\S+)/;
-        my $local = m/^\s+(\S+)\s*$/ ? $1 : next;
-        my $type  = pack_type $ns, $local;
-
-        substr($_, 1, 1)
-          = $self->{XCC_readers}{$type} ? 'R'
-          : $self->{XCC_dropts}{$type}  ? 'r' : ' ';
-
-        substr($_, 2, 1)
-          = $self->{XCC_writers}{$type} ? 'W'
-          : $self->{XCC_dwopts}{$type}  ? 'w' : ' ';
-    }
-    print $fh @output;
-}
-
-#----------------------
 
 
 sub reader($@)
@@ -290,6 +206,90 @@ sub _need($)
    : $_[1] eq 'WRITER' ? (0,1)
    : $_[1] eq 'RW'     ? (1,1)
    : error __x"use READER, WRITER or RW, not {dir}", dir => $_[1];
+}
+
+#----------------------
+
+
+sub declare($$@)
+{   my ($self, $need, $names, @opts) = @_;
+    my $opts = @opts==1 ? shift @opts : \@opts;
+    $opts = [ %$opts ] if ref $opts eq 'HASH';
+
+    my ($need_r, $need_w) = $self->_need($need);
+
+    foreach my $name (ref $names eq 'ARRAY' ? @$names : $names)
+    {   my $type = $self->findName($name);
+        if($need_r)
+        {   defined $self->{XCC_dropts}{$type}
+               and warning __x"reader {name} declared again", name => $name;
+            $self->{XCC_dropts}{$type} = $opts;
+        }
+
+        if($need_w)
+        {   defined $self->{XCC_dwopts}{$type}
+               and warning __x"writer {name} declared again", name => $name;
+            $self->{XCC_dwopts}{$type} = $opts;
+        }
+    }
+
+    $self;
+}
+
+
+sub findName($)
+{   my ($self, $name) = @_;
+    my ($type, $ns, $local);
+    if($name =~ m/^\{/)
+    {   ($type, $ns, $local) = ($name, unpack_type $name);
+    }
+    elsif($name =~ m/^(\w+)\:(\S+)$/)
+    {   (my $prefix, $local) = ($1, $2);
+        $ns = $self->{XCC_prefix}{$prefix}
+            or error __x"unknown protocol name prefix `{prefix}'"
+                 , prefix => $prefix;
+
+        $type  = pack_type $ns, $local;
+    }
+    else
+    {   error __x"protocol name must show namespace or prefix";
+    }
+
+    $type;
+}
+
+
+sub printIndex(@)
+{   my $self = shift;
+    my $fh   = @_ % 2 ? shift : select;
+    my %args = @_;
+    my $decl = exists $args{show_declared} ? delete $args{show_declared} : 1;
+
+    return $self->SUPER::printIndex($fh, %args)
+        unless $decl;
+
+    my $output = '';
+    open my($out), '>', \$output;
+
+    $self->SUPER::printIndex($out, %args);
+
+    close $out;
+    my @output = split /(?<=\n)/, $output;
+    my $ns     = '';
+    foreach (@output)
+    {   $ns = $1 if m/^namespace\:\s+(\S+)/;
+        my $local = m/^\s+(\S+)\s*$/ ? $1 : next;
+        my $type  = pack_type $ns, $local;
+
+        substr($_, 1, 1)
+          = $self->{XCC_readers}{$type} ? 'R'
+          : $self->{XCC_dropts}{$type}  ? 'r' : ' ';
+
+        substr($_, 2, 1)
+          = $self->{XCC_writers}{$type} ? 'W'
+          : $self->{XCC_dwopts}{$type}  ? 'w' : ' ';
+    }
+    print $fh @output;
 }
 
 1;
